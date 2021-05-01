a0bed5699a99bf6ac09fc8e691ac6301706f3232
ViniSilva32 <78766084+ViniSilva32@users.noreply.github.com>
2021-04-30 22:39:51 -0300

    Flock
    Flock  30/04/21

---
+++ Flock/Assets/Flock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flock : MonoBehaviour
+{
+    public FlockingManager myManager;
+    float speed;
+
+    void Start()
+    {
+        speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * speed);
+    }
+}
